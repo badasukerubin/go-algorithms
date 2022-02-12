45be0db6faad11eb9c48fc9f845587cd17962e3c
badasukerubin <kelvinvip8@gmail.com>
2022-02-12 11:27:37 +0100

    static and dynamic arrays: added delete, mutable values

--- data_structures/arrays/static_and_dynamic_arrays.go
+++ data_structures/arrays/static_and_dynamic_arrays.go
@@ -13,17 +13,17 @@ Slices solve this by letting us define arrays without lengths.
 Values can be added to an array
 O(1) - constant time
 */
-func push(array *[]string) {
-	*array = append(*array, "second") // O(1)
+func push(array *[]string, data string) {
+	*array = append(*array, data) // O(1)
 }
 
 /**
 Values can be added to the first array (unshift)
 O(n) - linear time
 */
-func unshift(array *[]string) {
+func unshift(array *[]string, data string) {
 	var x = make([]string, len(*array)+1) // We define an array x with the length of our slice plus an extra length to cater for the incoming string we intend to push to the first index.
-	x[0] = "first"
+	x[0] = data
 
 	for i := 0; i < len(*array); i++ { // O(n)
 		x[i+1] = (*array)[i]
@@ -41,12 +41,23 @@ func pop(array *[]string) {
 	// This method changes the order of the array
 }
 
+/**
+Values can be removed to an array by index
+O(1) - constant time
+*/
+func delete(array *[]string, index int) {
+	(*array)[index] = (*array)[len(*array)-1] // We replace the element we want to delete with the one at the end of the slice
+	pop(array)                                // O(1)
+}
+
 func main() {
 	array := []string{} // We define a slice, length is mutable by the compiler
-	push(&array)
+	push(&array, "second")
 	fmt.Printf("push: %v", array)
-	unshift(&array)
+	unshift(&array, "first")
 	fmt.Printf("\nunshift: %v", array)
+	delete(&array, 1)
+	fmt.Printf("\ndelete: %v", array)
 	pop(&array)
 	fmt.Printf("\npop: %v", array)
 }
