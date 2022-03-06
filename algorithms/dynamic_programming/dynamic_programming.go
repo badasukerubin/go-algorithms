ba3daf298a3bc0eefdee568d4d241e3812b55aa8
badasukerubin <kelvinvip8@gmail.com>
2022-03-06 20:18:37 +0100

    added dynamic programming for fibonacci sequence

--- algorithms/dynamic_programming/dynamic_programming.go
+++ algorithms/dynamic_programming/dynamic_programming.go
@@ -27,6 +27,41 @@ func memoizedAddTo80() func(int) int {
 
 }
 
+// O(2^n)
+// From algorithms/recursion/exercises/fibonacci/fibonacci.go
+var calculations int
+
+func fibonacciRecursive(number int) int {
+	calculations++
+	if number < 2 {
+		return number
+	}
+
+	return fibonacciRecursive(number-1) + fibonacciRecursive(number-2)
+}
+
+var calculationsMem int
+
+func fibonacciMemoized(n int) int {
+	cache := make(map[int]int)
+
+	return fib(n, cache)
+}
+
+func fib(n int, cache map[int]int) int {
+	calculationsMem++
+	if val, ok := cache[n]; ok {
+		return val
+	} else {
+		if n < 2 {
+			return n
+		} else {
+			cache[n] = fib(n-1, cache) + fib(n-2, cache)
+			return cache[n]
+		}
+	}
+}
+
 func main() {
 	// Calling addTo80 multiple times consumes time for each process; especially terrible if it's a long process
 	addTo80(5)
@@ -38,4 +73,13 @@ func main() {
 	memoized(5)
 	memoized(5)
 	memoized(5)
+
+	fibonacciRecursive(20)
+	// From the result of calculations, we can see that it drastically increases in relation to the fibonacci number - try 100 💀. This is rather inefficient considering the fact that we could cache the results
+	// fib(10) => 177 operations, f(20) => 21891 operations
+	fmt.Println(calculations)
+
+	_ = fibonacciMemoized(20)
+	// fib(10) => 19 operation, f(20) => 39 operations 💀
+	fmt.Println(calculationsMem)
 }
