a2aca09a949c2951950bcefd52ee8d5608178ceb
badasukerubin <kelvinvip8@gmail.com>
2022-05-10 01:51:44 +0100

    wip:smallest_positive_integer_in_array

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/badasukerubin/go-algorithms/furtherpractice"
+)
 
 func main() {
 	fmt.Print("Here's to a quite deeper knowledge on data structures and algorithms")
+	furtherpractice.Solution()
 }
